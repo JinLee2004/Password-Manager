6ffe0514c6c40eaaaae0f9f45e684a0c259d2dc7
Jin Lee <jin.g.lee@student.curtin.edu.au>
2023-11-21 14:03:22 +0800

    Added view function to code.

--- password_manager.py
+++ password_manager.py
@@ -2,19 +2,24 @@ master_pwd = input("What is the master password? ")
 menu = True
 
 def view():
-     
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, view_pass = data.split("|")
+            print("User:", user, "\nPassword:", view_pass)
+
 def add():
     name = input("Account name: ")
     pwd = input("Password: ")
     #with keyword closes file after use
     with open("passwords.txt, ", "a") as f:
-        f.write(name + "|" + pwd)
+        f.write(name + "|" + pwd + "\n")
 
 
 while menu == True:
     mode = input("Would you like to add a new password or view existing ones? \n"
                 "1. View\n"
-                "2. Add ")
+                "2. Add\n")
     if mode == "1":
             pass
     elif mode == "2":
